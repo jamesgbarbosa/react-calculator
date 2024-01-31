90bb57701246cea968fc054990e946b66a2f027c
james <james.barbosa@gmail.com>
2024-01-31 19:33:27 +0800

    Updated structure and styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,38 @@
 
 
+import "./styles.css"
+
 export default function Home() {
   return (
     <main>
       <div className="calculator">
         <div className="output">
           <div className="previous-op">
-
+            123123123
           </div>
           <div className="current-op">
-
+123123123123
           </div>
-          <button className="span-2">AC</button>
-          <button>DEL</button>
-          <button>/</button>
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>*</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>+</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>-</button>
-          <button>.</button>
-          <button>0</button>
-          <button className="span-2">=</button>
+
         </div>
+        <button className="span-2">AC</button>
+        <button>DEL</button>
+        <button>/</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>*</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>+</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>-</button>
+        <button>.</button>
+        <button>0</button>
+        <button className="span-2">=</button>
       </div>
     </main>
   );
